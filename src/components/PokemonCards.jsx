658adc8a8c5b411394aab138eaf5b27121e87756
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-02 13:57:57 +0100

    problem

---
+++ src/components/PokemonCards.jsx
@@ -0,0 +1,14 @@
+const PokemonCards = () => {
+    return ( 
+        <div className="pokemonCards">
+            <img src="" alt="" />
+            <div>
+                <h1>HAllo</h1>
+                <p>Lorem ipsum dolor sit amet consectet</p>
+                <p></p>
+            </div>
+        </div>
+     );
+}
+ 
+export default PokemonCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,29 @@
+import {useState, useEffect} from 'react'
+import PokemonCards from '../components/PokemonCards.jsx'
+
 const Home = () => {
+    
+    const [pokemon,setPokemon] = useState([])
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`)
+        .then(res => res.json())
+        .then(data => {
+            setPokemon(data.results)
+        })
+    },[])
+    
+
     return ( 
-        <h1>Home</h1>
+        <main>
+            <h1>Home</h1>
+
+            {pokemon.map((items) => {
+                <PokemonCards 
+                name={items.name}
+                />
+            })}
+        </main>
      );
 }
  

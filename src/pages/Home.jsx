d42adcd19f77540682f223d8ef34a9fac1c6ac95
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-03 13:49:55 +0100

    conditional rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 const Home = () => {
     
     const [pokemon,setPokemon] = useState([])
+    const [display,setDisplay] = useState (true)
 
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`)
@@ -15,26 +16,40 @@ const Home = () => {
         })
     },[])
     
-
+if(display){
 
     return ( 
         <main>
+            <button onClick={() => setDisplay(false)}>types</button>
             <h1>Home</h1>
                 {pokemon.map((items,index) => {
                 return(
-
+    
                     <PokemonCards 
                     key={uuidv4()}
                     name={items.name}
                     url={items.url}
                     id={index + 1}
                     />
-
+    
                 )
             })}
            
         </main>
      );
+
+}
+else if (!display){
+    
+    return(
+        <div>
+            <h1>Hallo</h1>
+            <button onClick={() => setDisplay(true)}>X</button>
+        </div>
+
+    )
+}
+ 
 }
  
 export default Home;

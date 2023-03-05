a01d5fa1e50c8bf5c485fc0d6c6df97d787f436b
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-05 01:21:54 +0100

    type filter working with buttons

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -4,11 +4,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Testpage = () => {
     
-    const [pokemonName,setPokemonName] = useState([])
+    const [pokemonFiltered,setPokemonFiltered] = useState([])
+    
+    
     const [basicData,setBasicData] = useState([])
-    const [searchedType,setSearchedType] = useState("")
+    const [searchedType,setSearchedType] = useState("grass")
     const [filteredData, setFilteredData] = useState([])
     const filteredListArr = []
+    const [display,setDisplay] = useState(false)
     
 
         useEffect(() => {
@@ -16,7 +19,7 @@ const Testpage = () => {
 
         const filteredTypesArr = []
         let arrLength
-        let searchedType = "water"
+        /* let searchedType = "water" */
         
         fetch(`https://pokeapi.co/api/v2/type/${searchedType}`)
         .then(res => res.json())
@@ -30,7 +33,7 @@ const Testpage = () => {
                 /* console.log(data.pokemon) */
                 
             })
-            setPokemonName(filteredTypesArr)
+            setPokemonFiltered(filteredTypesArr)
             arrLength = filteredTypesArr.length
             console.log(filteredTypesArr)
 
@@ -48,34 +51,34 @@ const Testpage = () => {
         
     },[searchedType])
     
- 
-
-/*     filteredData.map((item,index) => {
-        console.log(item.pokemon.url)
- 
-
-
-    }) */
-
-    /* console.log(filteredListArr) */
-    
-
+if(!display){
     return ( 
 
         <main>
 
-
-                {pokemonName.map((items,index) => {
-                    return(
-                            <PokemonCards 
-                            key={uuidv4()}
-                            name={items.name}
-                            url={items.url}
-                            id={index + 1}
-                            />
-                    )
-                })}
-{/*                 {basicData.map((items,index) => {
+<button onClick={() => {setSearchedType("bug") , setDisplay(true)}}>Bug</button>
+<button onClick={() => {setSearchedType("dark"), setDisplay(true)}}>Dark</button>
+<button onClick={() => {setSearchedType("dragon") ,setDisplay(true)}}>Dragon</button>
+<button onClick={() => {setSearchedType("electric"), setDisplay(true)}}>Electric</button>
+<button onClick={() => {setSearchedType("fairy") ,setDisplay(true)}}>Fairy</button>
+<button onClick={() => {setSearchedType("fighting") ,setDisplay(true)}}>Fighting</button>
+<button onClick={() => {setSearchedType("fire"), setDisplay(true)}}>Fire</button>
+<button onClick={() => {setSearchedType("flying"), setDisplay(true)}}>Flying</button>
+<button onClick={() => {setSearchedType("ghost"), setDisplay(true)}}>Ghost</button>
+<button onClick={() => {setSearchedType("grass") ,setDisplay(true)}}>Grass</button>
+<button onClick={() => {setSearchedType("ground"), setDisplay(true)}}>Ground</button>
+<button onClick={() => {setSearchedType("ice"), setDisplay(true)}}>Ice</button>
+<button onClick={() => {setSearchedType("normal") ,setDisplay(true)}}>Normal</button>
+<button onClick={() => {setSearchedType("plant"), setDisplay(true)}}>Plant</button>
+<button onClick={() => {setSearchedType("poison") ,setDisplay(true)}}>Poison</button>
+<button onClick={() => {setSearchedType("psychic") ,setDisplay(true)}}>Psychic</button>
+<button onClick={() => {setSearchedType("rock"), setDisplay(true)}}>Rock</button>
+<button onClick={() => {setSearchedType("steel"), setDisplay(true)}}>Steel</button>
+<button onClick={() => {setSearchedType("water") ,setDisplay(true)}}>Water</button>
+                {/* =========== pokemonFiltered ===========> gibt gefilterte Liste zurück
+                    =========== basicData       ===========> gibt ungefilterte Liste zurück
+                */}
+{/*                 {pokemonFiltered.map((items,index) => {
                     return(
                             <PokemonCards 
                             key={uuidv4()}
@@ -85,8 +88,59 @@ const Testpage = () => {
                             />
                     )
                 })} */}
+            {basicData.map((items,index) => {
+                    return(
+                            <PokemonCards 
+                            key={uuidv4()}
+                            name={items.name}
+                            url={items.url}
+                            id={index + 1}
+                            />
+                    )
+                })} 
             </main>
-     );
+     )
+}else {
+
+    return(
+        <main>
+<button onClick={() => {setSearchedType("bug") , setDisplay(true)}}>Bug</button>
+<button onClick={() => {setSearchedType("dark"), setDisplay(true)}}>Dark</button>
+<button onClick={() => {setSearchedType("dragon") ,setDisplay(true)}}>Dragon</button>
+<button onClick={() => {setSearchedType("electric"), setDisplay(true)}}>Electric</button>
+<button onClick={() => {setSearchedType("fairy") ,setDisplay(true)}}>Fairy</button>
+<button onClick={() => {setSearchedType("fighting") ,setDisplay(true)}}>Fighting</button>
+<button onClick={() => {setSearchedType("fire"), setDisplay(true)}}>Fire</button>
+<button onClick={() => {setSearchedType("flying"), setDisplay(true)}}>Flying</button>
+<button onClick={() => {setSearchedType("ghost"), setDisplay(true)}}>Ghost</button>
+<button onClick={() => {setSearchedType("grass") ,setDisplay(true)}}>Grass</button>
+<button onClick={() => {setSearchedType("ground"), setDisplay(true)}}>Ground</button>
+<button onClick={() => {setSearchedType("ice"), setDisplay(true)}}>Ice</button>
+<button onClick={() => {setSearchedType("normal") ,setDisplay(true)}}>Normal</button>
+<button onClick={() => {setSearchedType("plant"), setDisplay(true)}}>Plant</button>
+<button onClick={() => {setSearchedType("poison") ,setDisplay(true)}}>Poison</button>
+<button onClick={() => {setSearchedType("psychic") ,setDisplay(true)}}>Psychic</button>
+<button onClick={() => {setSearchedType("rock"), setDisplay(true)}}>Rock</button>
+<button onClick={() => {setSearchedType("steel"), setDisplay(true)}}>Steel</button>
+<button onClick={() => {setSearchedType("water") ,setDisplay(true)}}>Water</button>
+            
+            {pokemonFiltered.map((items,index) => {
+            return(
+                    <PokemonCards 
+                    key={uuidv4()}
+                    name={items.name}
+                    url={items.url}
+                    id={index + 1}
+                    />
+            )
+        })}
+
+        </main>
+
+
+    )
+
+}
 }
  
 export default Testpage;

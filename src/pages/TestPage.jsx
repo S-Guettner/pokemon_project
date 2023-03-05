b4ee498743e3c367d5d955fe78106b2a8c704e00
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-05 01:07:14 +0100

    type filter working

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -7,7 +7,8 @@ const Testpage = () => {
     const [pokemonName,setPokemonName] = useState([])
     const [basicData,setBasicData] = useState([])
     const [searchedType,setSearchedType] = useState("")
-
+    const [filteredData, setFilteredData] = useState([])
+    const filteredListArr = []
     
 
         useEffect(() => {
@@ -15,76 +16,65 @@ const Testpage = () => {
 
         const filteredTypesArr = []
         let arrLength
-        /* let searchedType = "grass" */
+        let searchedType = "water"
         
         fetch(`https://pokeapi.co/api/v2/type/${searchedType}`)
         .then(res => res.json())
         .then(data => {
             
+            setFilteredData(data.pokemon)
+            
+
             data.pokemon.map((item) => {
-                filteredTypesArr.push(item.pokemon.name)
+                filteredTypesArr.push(item.pokemon)
+                /* console.log(data.pokemon) */
                 
             })
             setPokemonName(filteredTypesArr)
             arrLength = filteredTypesArr.length
+            console.log(filteredTypesArr)
 
 
 
             fetch(`https://pokeapi.co/api/v2/pokemon?limit=${arrLength}&offset=0`)
             .then(res => res.json())
             .then(data => {
-/*                 console.log(data.results) */
                 setBasicData(data.results)
+                console.log(data.results)
             })
         })
 
         
+        
     },[searchedType])
     
-    pokemonName.map((item) => {
-        console.log(item)
+ 
+
+/*     filteredData.map((item,index) => {
+        console.log(item.pokemon.url)
+ 
 
 
-    })
+    }) */
 
-    
+    /* console.log(filteredListArr) */
     
 
     return ( 
 
         <main>
-                        <button onClick={() => setSearchedType("bug")}>Bug</button>
-                        <button onClick={() => setSearchedType("dark")}>Dark</button>
-                        <button onClick={() => setSearchedType("dragon")}>Dragon</button>
-                        <button onClick={() => setSearchedType("electric")}>Electric</button>
-                        <button onClick={() => setSearchedType("fairy")}>Fairy</button>
-                        <button onClick={() => setSearchedType("fighting")}>Fighting</button>
-                        <button onClick={() => setSearchedType("fire")}>Fire</button>
-                        <button onClick={() => setSearchedType("flying")}>Flying</button>
-                        <button onClick={() => setSearchedType("ghost")}>Ghost</button>
-                        <button onClick={() => setSearchedType("grass")}>Grass</button>
-                        <button onClick={() => setSearchedType("ground")}>Ground</button>
-                        <button onClick={() => setSearchedType("ice")}>Ice</button>
-                        <button onClick={() => setSearchedType("normal")}>Normal</button>
-                        <button onClick={() => setSearchedType("plant")}>Plant</button>
-                        <button onClick={() => setSearchedType("poison")}>Poison</button>
-                        <button onClick={() => setSearchedType("psychic")}>Psychic</button>
-                        <button onClick={() => setSearchedType("rock")}>Rock</button>
-                        <button onClick={() => setSearchedType("steel")}>Steel</button>
-                        <button onClick={() => setSearchedType("water")}>Water</button>
 
 
-                    {pokemonName.map((item) => {
-                        return(
-                        <div>
-                            <p>{item}</p>
-                        </div>
-                        
-                        )
-                    
-                    })}
-
-
+                {pokemonName.map((items,index) => {
+                    return(
+                            <PokemonCards 
+                            key={uuidv4()}
+                            name={items.name}
+                            url={items.url}
+                            id={index + 1}
+                            />
+                    )
+                })}
 {/*                 {basicData.map((items,index) => {
                     return(
                             <PokemonCards 
